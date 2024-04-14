802102549da2fd35d51a366dbce9b293b2d2299d
Ratchanon Traitiprat <98165333+manro7411@users.noreply.github.com>
2024-04-15 00:04:15 +0200

    Delete valuestorage_umbrella directory

--- valuestorage_umbrella/README.md
+++
@@ -1,4 +0,0 @@
-# ValuestorageUmbrella
-
-**TODO: Add description**
-
--- valuestorage_umbrella/apps/valuestorage/test/value_storage_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ValueStorageTest do
-  use ExUnit.Case
-  doctest ValueStorage
-
-  test "greets the world" do
-    assert ValueStorage.hello() == :world
-  end
-end
